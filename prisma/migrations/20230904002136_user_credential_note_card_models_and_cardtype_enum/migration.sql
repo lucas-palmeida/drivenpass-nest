7a0ada4237ae6bc246148a06f50e4fbe9e13977c
lucas-palmeida <lucaspacheco95@gmail.com>
2023-09-03 21:29:18 -0300

    feat: implement prisma schema and migration

---
+++ prisma/migrations/20230904002136_user_credential_note_card_models_and_cardtype_enum/migration.sql
@@ -0,0 +1,71 @@
+-- CreateEnum
+CREATE TYPE "CardType" AS ENUM ('CREDIT', 'DEBIT', 'CREDIT_DEBIT');
+
+-- CreateTable
+CREATE TABLE "users" (
+    "id" SERIAL NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "credentials" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "url" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "userId" INTEGER NOT NULL,
+
+    CONSTRAINT "credentials_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "notes" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "userId" INTEGER NOT NULL,
+
+    CONSTRAINT "notes_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "cards" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "number" TEXT NOT NULL,
+    "holder" TEXT NOT NULL,
+    "cvv" TEXT NOT NULL,
+    "expiryMonth" INTEGER NOT NULL,
+    "expiryYear" INTEGER NOT NULL,
+    "password" TEXT NOT NULL,
+    "isVirtual" BOOLEAN NOT NULL DEFAULT false,
+    "cardType" "CardType" NOT NULL,
+    "userId" INTEGER NOT NULL,
+
+    CONSTRAINT "cards_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "credentials_title_userId_key" ON "credentials"("title", "userId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "notes_title_userId_key" ON "notes"("title", "userId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "cards_title_userId_key" ON "cards"("title", "userId");
+
+-- AddForeignKey
+ALTER TABLE "credentials" ADD CONSTRAINT "credentials_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "notes" ADD CONSTRAINT "notes_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "cards" ADD CONSTRAINT "cards_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
